ed1440ee7bd51bc59ca4b74d446c91900b585bfd
JoshuaCorrigan <1110112@apps.nsd.org>
2022-05-11 11:37:31 -0700

    Create Constants.java
    Constants class created for ease of use with static values to reference in other classes

---
+++ Constants.java
@@ -0,0 +1,7 @@
+public class Constants{
+    public static final String[] SUITES   = {"♠","♥","♦","♣"};
+    public static final String[] SPECIALS = {"A","J","Q","K"};
+    public static final int BASE          = 9;
+    public static final int DECK_SIZE = (BASE+SPECIALS.length)*SUITES.length; 
+    
+  }
