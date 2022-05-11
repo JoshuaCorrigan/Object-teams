902193bb83a91960a1c9259b0a7b3325520567d7
JoshuaCorrigan <1110112@apps.nsd.org>
2022-05-11 11:49:07 -0700

    conflict test

--- bjCard.java
+++ bjCard.java
@@ -1,3 +1,5 @@
 public class bjCard implements Card {
-    
+private static final String[] SUITES   = Constants.SUITES;
+  private static final String[] SPECIALS = Constants.SPECIALS;
+  private static final int BASE          = Constants.BASE;
 }
