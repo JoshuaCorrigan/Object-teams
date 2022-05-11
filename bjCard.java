e01c4d73cec076a49d9b919d0f5bf2856aa774a1
Gu-Jack <1097549@apps.nsd.org>
2022-05-11 12:05:08 -0700

    Format

--- bjCard.java
+++ bjCard.java
@@ -1,13 +1,20 @@
 public class bjCard implements Card {
-private static final String[] SUITS   = Constants.SUITS;
-  private static final String[] SPECIALS = Constants.SPECIALS;
-  private static final int BASE          = Constants.BASE;
-
+    private static final String[] SUITS     = Constants.SUITS;
+    private static final String[] SPECIALS  = Constants.SPECIALS;
+    private static final int BASE           = Constants.BASE;
 
     private int value;
-    private int suit;
+    private String suit;
 
     public bjCard(int seed) {
 
+    }
+    
+    public int getValue() {
+        return this.value;
+    }
+
+    public String getSUITS() {
+        return this.suit;
     }
 }
